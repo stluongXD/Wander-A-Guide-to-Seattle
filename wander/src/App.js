c3fa18c5b8dea9ffec0e2d8a6525ff4a72946f60
Damita Gomez <damita.gomez@comcast.net>
2021-03-01 19:35:14 -0800

    Draft of Home Page

--- wander/src/App.js
+++ wander/src/App.js
@@ -1,14 +1,35 @@
 // Style
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // Club Components
-import WanderNavbar from './navigation/WanderNavbar.js';
+import WanderNavbar from './navigation/WanderNavbar';
+import Home from './home/HomePage';
+//import About from './about/About.js';
+//import Collections from './collections/collections.js';
 
 function App() {
-  return (
-    <WanderNavbar/>
-  );
-}
+    return (
+      <div>
+        <Router>
+          <WanderNavbar />
+          <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            {/*<Route exact path="/about"><About /></Route>*/}
+            {/*<Route exact path="/collections"><Collections /></Route>*/}
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+
 
 export default App;
